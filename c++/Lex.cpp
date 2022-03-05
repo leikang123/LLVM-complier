2d062f2c89dde8ed73be50d3e678fcfe029950fc
leikang123 <1013626230@qq.com>
2022-03-05 18:45:45 +0800

    add file

--- c++/Lex.cpp
+++ c++/Lex.cpp
@@ -40,3 +40,54 @@ id {(letter()|_} {{letter}|{digit}|_}*
 number  [1-9](digit)*|0
 %%
 {white}+;
+"/**{^]\n}***/"   /* 删除注释 */
+"//"{^}\n]*       /*删除注释 */
+{id} {fprint(yyout,"(%d,%s) \n",ID,yytext);}
+{unmber} {printf(yyout,"(%d,%s) \n",NUMBER,yytext);}
+{void} {printf(yyout,"(%d,%s) \n",VOID,yytetx);}
+{char} {printf(yyout,"(%d,%s)\n",CHAR,yytext);}
+{int} {printf(yyout,"(%d,%s) \n",INT,yytext);}
+{float} {printf(yyout,"(%d,%s) \n",FLOAT,yytext);}
+{double} {printf(yyout,"(%d,%s) \n",DOUBLE,yytext);}
+{short} {printf(yyout,"(%d,%s) \n",SHORT,yytext);}
+{long} {printf(yyout,"(%d,%s) \n",LONG,yytext);}
+{signed} {printf(yyout,"(%d,%s) \n",SINGED,yytext);}
+{unsigned} {printf(yyout,"(%d,%s) \n",UNSINGED,yytext);}
+{struct} {printf(yyout,"(%d,%s) \n",STRUCT,yytext);}
+{enum} {printf(yyout,"(%d,%s) \n",ENUM,yytext);}
+{union} {printf(yyout,"(%d,%s) \n",UNION,yytext);}
+{typedef} {printf(yyout,"(%d,%s) \n",TYPEDF,yytext);}
+{sizeof} {printf(yyout,"(%d,%s) \n",SIZEOF,yytext);}
+{long} {printf(yyout,"(%d,%s) \n",LONG,yytext);}
+{auto} {printf(yyout,"(%d,%s) \n",AUTO,yytext);}
+{register} {printf(yyout,"(%d,%s) \n",REGISTER,yytext);}
+{extern} {printf(yyout,"(%d,%s) \n",EXTERN,yytext);}
+{const} {printf(yyout,"(%d,%s) \n",CONST,yytext);}
+{volatile} {printf(yyout,"(%d,%s) \n",VOLATILE,yytext);}
+{return} {printf(yyout,"(%d,%s) \n",RETURN,yytext);}
+{continue} {printf(yyout,"(%d,%s) \n",CONTIUNE,yytext);}
+{break} {printf(yyout,"(%d,%s) \n",BREAK,yytext);}
+{goto} {printf(yyout,"(%d,%s) \n",GOTO,yytext);}
+{if} {printf(yyout,"(%d,%s) \n",IF,yytext);}
+{else} {printf(yyout,"(%d,%s) \n",ELSE,yytext);}
+{switch} {printf(yyout,"(%d,%s) \n",SIGWINCH,yytext);}
+{case} {printf(yyout,"(%d,%s) \n",CASE,yytext);}
+{for} {printf(yyout,"(%d,%s) \n",FOR,yytext);}
+{do} {printf(yyout,"(%d,%s) \n",DO,yytext);}
+{while} {printf(yyout,"(%d,%s) \n",WHILE,yytext);}
+{scanf} {printf(yyout,"(%d,%s) \n",SCANF,yytext);}
+{printf} {printf(yyout,"(%d,%s) \n",PRINTF,yytext);}
+"{" {printf(yyout,"(%d,%s) \n",LC,yytext);}
+"}" {printf(yyout,"(%d,%s) \n",RC,yytext);}
+"[" {printf(yyout,"(%d,%s)\n",LB,yytext);}
+"]" {printf(yyout,"(%d,%s) \n",RB,yytext);}
+"(" {printf(yyout,"(%d,%s) \n",LP,yytext);}
+")" {printf(yyout,"(%d,%s) \n",RP,yytext);}
+...
+%%
+main()     /*ZHUHANSHU*/
+{
+  yyin = fopen("test.txt","r");
+  yyout = fopen("result.txt","w");
+  if(yyin !=NULL) return yylex();
+}
